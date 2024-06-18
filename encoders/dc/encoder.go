0fea075d6fd593434d4ce1d2718093075988010e
netsensei <matthias@colada.be>
2024-06-18 17:09:42 +0200

    Changed: extending/adding dublin core fields

--- encoders/dc/encoder.go
+++ encoders/dc/encoder.go
@@ -17,15 +17,93 @@ var dc = template.Must(template.New("").Funcs(funcs).Parse(`
 	<dcterms:identifier>{{ .Identifier }}</dcterms:identifier>
 	
 	{{- if .Description.Title }}
-	<dcterms:title xml:lang="nl">{{ .Description.Title }}</dcterms:title>
+	<dcterms:title xml:lang="{{ .Description.Title.Lang }}">{{ .Description.Title.Value }}</dcterms:title>
 	{{- end }}
 	
 	{{- if .Description.Description }}
-	<dcterms:description xml:lang="nl">{{ .Description.Description }}</dcterms:description>
+	<dcterms:description xml:lang="{{ .Description.Description.Lang }}">{{ .Description.Description.Value }}</dcterms:description>
 	{{- end }}
 	
-	{{- if .Description.Created}}
+	{{- if .Description.Created }}
 	<dcterms:created xsi:type="edtf:EDTF-level1">{{ .Description.Created }}</dcterms:created>
+	{{- end }}
+
+	{{- if .Description.Alternative }}
+	{{ range .Description.Alternative }}
+	<dcterms:alternative xml:lang="nl">{{ . }}</dcterms:alternative>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.Extent }}
+	<dcterms:extent>{{ .Description.Extent }}</dcterms:extent>
+	{{- end }}
+
+	{{- if .Description.Available }}
+	<dcterms:available>{{ .Description.Available }}</dcterms:available>
+	{{- end }}
+
+	{{- if .Description.Abstract }}
+	<dcterms:abstract xml:lang="{{ .Description.Abstract.Lang }}">{{ .Description.Abstract.Value }}</dcterms:abstract>
+	{{- end }}
+
+	{{- if .Description.Issued }}
+	<dcterms:issued xsi:type="edtf:EDTF-level1">{{ .Description.Issued }}</dcterms:issued>
+	{{- end }}
+
+	{{- if .Description.Publisher }}
+	{{ range .Description.Publisher }}
+	<dcterms:publisher>{{ . }}</dcterms:publisher>
+	{{ end }}
+	{{- end}}
+
+	{{- if .Description.Contributor }}
+	{{ range .Description.Contributor }}
+	<dcterms:contributor>{{ . }}</dcterms:contributor>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.Spatial }}
+	{{ range .Description.Spatial }}
+	<dcterms:spatial>{{ . }}</dcterms:spatial>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.Temporal }}
+	{{ range .Description.Temporal }}
+	<dcterms:temporal>{{ . }}</dcterms:temporal>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.Subject }}
+	{{ range .Description.Subject }}
+	<dcterms:subject xml:lang="{{ .Description.Subject.Lang }}">{{ .Description.Subject.Value }}</dcterms:subject>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.Language }}
+	{{ range .Description.Language }}
+	<dcterms:language>{{ . }}</dcterms:language>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.License }}
+	{{ range .Description.License }}
+	<dcterms:license>{{ . }}</dcterms:license>
+	{{ end }}
+	{{- end }}
+
+	{{- if .Description.RightsHolder }}
+	<dcterms:rightsHolder>{{ .Description.RightsHolder }}</dcterms:rightsHolder>
+	{{- end }}
+
+	{{- if .Description.Rights }}
+	<dcterms:rights xml:lang="nl">{{ .Description.Rights }}</dcterms:rights>
+	{{- end }}
+
+	{{- if .Description.Type }}
+	{{ range .Description.Type }}
+	<dcterms:type>{{ . }}</dcterms:type>
+	{{ end }}
 	{{- end }}
 </metadata>
 {{ end}}
--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,8 +1,29 @@
 package metadata
 
 type DublinCore struct {
-	Identifier  string `json:"identifier"`
-	Title       string `json:"string"`
-	Description string `json:"description"`
-	Created     string `json:"created"`
+	Title        Text
+	Alternative  []string `json:"alternative"`
+	Identifier   string
+	Extent       string   `json:"extent"`
+	Available    string   `json:"available"`
+	Description  Text     `json:"description,omitempty"`
+	Abstract     Text     `json:"abstract"`
+	Created      string   `json:"created"`
+	Issued       string   `json:"issued"`
+	Publisher    []string `json:"publisher"`
+	Contributor  []string `json:"contributor"`
+	Creator      []string `json:"creator"`
+	Spatial      []string `json:"spatial"`
+	Temporal     []string `json:"temporal"`
+	Subject      []Text   `json:"subject"`
+	Language     []string `json:"language"`
+	License      []string `json:"license"`
+	RightsHolder string   `json:"rightsHolder"`
+	Rights       string   `json:"rights"`
+	Type         []string `json:"type"`
+}
+
+type Text struct {
+	Value string `json:"value"`
+	Lang  string `json:"lang"`
 }
